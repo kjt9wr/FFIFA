e51badf4fc012773519f49cc6624558e900f27c6
Kevin Thumim <kevin.j.thum@gmail.com>
2020-03-29 17:22:19 -0400

    retreive owner info from db

--- src/components/roster.component.js
+++ src/components/roster.component.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
-export default class HomePage extends Component {
+
+export default class Roster extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            owner: null
+        };
+      }
+      
+    // Get Owner from DB
+    componentDidMount() {
+        axios.get('http://localhost:5000/owner/')
+          .then(response => {
+            const { name } = this.props.match.params
+            const owner = response.data.find(curruser => name === curruser.name);
+            this.setState({owner})
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+
     render() {
         const { name } = this.props.match.params
         return (
