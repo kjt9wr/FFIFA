738438aea2d74cbd7eed886057b10c91bb3c97b5
Kevin Thumim <kevin.j.thum@gmail.com>
2024-01-21 02:19:15 -0500

    remove admin page for staging

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,39 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Button, Container } from "reactstrap";
-import { SLEEPER_LEAGUE_ID } from "../Utilities/Constants";
+import React from "react";
+import { Container } from "reactstrap";
 
 const Admin = () => {
-  const [roster, setRoster] = useState({ players: [] });
-
-  useEffect(() => {
-    const getDraftInfo = async () => {
-      const draftInfo = await axios.get(
-        `https://api.sleeper.app/v1/league/${SLEEPER_LEAGUE_ID}/rosters`
-      );
-      const alexRoster = draftInfo.data.filter(
-        (roster) => roster.roster_id === 1
-      );
-      setRoster(alexRoster[0]);
-    };
-
-    getDraftInfo();
-  }, []);
-
-  const alexID = "5e80dd6ab3bdaf34133161bd";
-
-  const updateAlexRoster = async () => {
-    await axios.post(`http://localhost:5000/player/update/roster/${alexID}`, {
-      players: roster.players,
-    });
-  };
-
   return (
     <Container>
       <h1 className="text-center"> Admin Page </h1>
-      <Button title="Update Alex Roster" onClick={updateAlexRoster}>
-        Update Alex Roster
-      </Button>
+      {/* TODO */}
     </Container>
   );
 };
