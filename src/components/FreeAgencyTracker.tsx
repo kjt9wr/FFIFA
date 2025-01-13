a05ec6246acbece9342a02cee4ddda38376417ab
Kevin Thumim <kevin.j.thum@gmail.com>
2025-01-12 23:24:40 -0500

    create free agency custom hook

--- src/components/FreeAgencyTracker.tsx
+++ src/components/FreeAgencyTracker.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Row, Table } from "reactstrap";
+import { useFreeAgents } from "../custom-hooks/custom-hooks";
+import { Player } from "../interfaces/interfaces";
 import * as Constants from "../utilities/constants";
-import { fetchFreeAgents } from "../api/api.service";
-import { Player, FreeAgentStorage } from "../interfaces/interfaces";
 
 interface FreeAgentColumnProps {
   availablePlayers: Player[];
@@ -27,48 +27,11 @@ const FreeAgentColumn = (props: FreeAgentColumnProps) => {
   );
 };
 
-const getFreeAgentsFromDB = async () => {
-  const availablePlayers = await fetchFreeAgents();
-
-  const availableQBs: Player[] = availablePlayers.data
-    .filter(
-      (player: Player) => player.position === Constants.QB && player.rank < 80
-    )
-    .sort((a: Player, b: Player) => a.rank - b.rank);
-  const availableRBs: Player[] = availablePlayers.data
-    .filter(
-      (player: Player) => player.position === Constants.RB && player.rank < 80
-    )
-    .sort((a: Player, b: Player) => a.rank - b.rank);
-  const availableWRs: Player[] = availablePlayers.data
-    .filter(
-      (player: Player) => player.position === Constants.WR && player.rank < 80
-    )
-    .sort((a: Player, b: Player) => a.rank - b.rank);
-  const availableTEs: Player[] = availablePlayers.data
-    .filter(
-      (player: Player) => player.position === Constants.TE && player.rank < 80
-    )
-    .sort((a: Player, b: Player) => a.rank - b.rank);
-
-  return { availableQBs, availableRBs, availableWRs, availableTEs };
-};
-
 /*
  * This page displays all free agent players organized by position
  */
 const FreeAgency = () => {
-  const [freeAgents, setFreeAgents] = useState<FreeAgentStorage>();
-
-  useEffect(() => {
-    const getFreeAgents = async () => {
-      const allFreeAgents = await getFreeAgentsFromDB();
-      setFreeAgents(allFreeAgents);
-    };
-
-    getFreeAgents();
-  }, []);
-
+  const freeAgents = useFreeAgents();
   return (
     <div>
       <h2 className="text-center"> Free Agents </h2>
--- src/custom-hooks/custom-hooks.ts
+++ src/custom-hooks/custom-hooks.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import { fetchFreeAgents } from "../api/api.service";
+import { FreeAgentStorage, Player } from "../interfaces/interfaces";
 import * as FranchiseService from "../services/franchise.service";
+import * as Constants from "../utilities/constants";
 
 export const useFranchiseInfo = () => {
   const [franchiseInfo, setFranchiseInfo] = useState({});
@@ -14,4 +17,56 @@ export const useFranchiseInfo = () => {
   }, []);
 
   return franchiseInfo;
+};
+
+const getAvailablePlayersPosition = (
+  position: string,
+  playerList: Player[]
+) => {
+  return playerList
+    .filter(
+      (player: Player) => player.position === position && player.rank < 80
+    )
+    .sort((a: Player, b: Player) => a.rank - b.rank);
+};
+
+export const useFreeAgents = () => {
+  const [freeAgents, setFreeAgents] = useState<FreeAgentStorage>();
+
+  useEffect(() => {
+    const getFreeAgents = async () => {
+      const availablePlayers = await fetchFreeAgents();
+
+      const availableQBs = getAvailablePlayersPosition(
+        Constants.QB,
+        availablePlayers.data
+      );
+
+      const availableRBs = getAvailablePlayersPosition(
+        Constants.RB,
+        availablePlayers.data
+      );
+      const availableWRs = getAvailablePlayersPosition(
+        Constants.WR,
+        availablePlayers.data
+      );
+      const availableTEs = getAvailablePlayersPosition(
+        Constants.TE,
+        availablePlayers.data
+      );
+
+      const allFreeAgents = {
+        availableQBs,
+        availableRBs,
+        availableWRs,
+        availableTEs,
+      };
+
+      setFreeAgents(allFreeAgents);
+    };
+
+    getFreeAgents();
+  }, []);
+
+  return freeAgents;
 };
