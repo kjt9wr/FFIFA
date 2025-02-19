0f7e77c07e46b97699cf877ce846e14b356b824b
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-19 17:02:11 -0500

    server.js formatting

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,22 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const ownerRouter = require('./routes/owner');
-const playerRouter = require('./routes/player');
-const tradeRouter = require('./routes/trade');
+const ownerRouter = require("./routes/owner");
+const playerRouter = require("./routes/player");
+const tradeRouter = require("./routes/trade");
 
-app.use('/owner', ownerRouter);
-app.use('/player', playerRouter);
-app.use('/trade/', tradeRouter);
-
+app.use("/owner", ownerRouter);
+app.use("/player", playerRouter);
+app.use("/trade/", tradeRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
