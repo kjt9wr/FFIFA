84c2aa4f553ef376fef8ddb331ddacc272453662
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-19 17:32:52 -0500

    clean up server.js formatting even more

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
+const ownerRouter = require("./routes/owner");
+const playerRouter = require("./routes/player");
+const tradeRouter = require("./routes/trade");
 
+// express app
 const app = express();
-const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Handle DB Connection
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -21,14 +27,13 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const ownerRouter = require("./routes/owner");
-const playerRouter = require("./routes/player");
-const tradeRouter = require("./routes/trade");
-
+// Routes
 app.use("/owner", ownerRouter);
 app.use("/player", playerRouter);
 app.use("/trade/", tradeRouter);
 
+// listen for requests
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
