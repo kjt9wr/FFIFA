2661ebc65dc7bba2b27e9e18f59915f0acc06200
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-20 18:48:06 -0500

    add middleware to log api calls

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${req.method}] `, req.path);
+  next();
+});
 
 // Handle DB Connection
 const uri = process.env.ATLAS_URI;
