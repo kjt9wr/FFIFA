e91f0c589d25d32c2c7e17f9e02bd87d16747419
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-20 19:03:07 -0500

    create owner.controller for owner route logic

---
+++ backend/controllers/owner.controller.js
@@ -0,0 +1,78 @@
+const Owner = require("../models/owner.model");
+const idMap = require("../utils/idMaps.js");
+
+// POST a new owner
+const createOwner = async (req, res) => {
+  const { name, cap, sleeperId, active } = req.body;
+
+  Owner.create({
+    name,
+    cap,
+    sleeperId,
+    active,
+  })
+    .then(() =>
+      res.status(201).json({
+        message: "Owner created successfully!",
+        owner: {
+          name,
+          cap,
+          sleeperId,
+          active,
+        },
+      })
+    )
+    .catch((err) => res.status(400).json("Unable to add owner: " + err));
+};
+
+// GET all owners
+const getOwners = async (req, res) => {
+  Owner.find()
+    .then((owner) => res.json(owner))
+    .catch((err) => res.status(400).json("Unable to find owner: " + err));
+};
+
+// GET a single owner by name
+const getOwnerByName = async (req, res) => {
+  Owner.find({ name: req.params.name })
+    .then((owner) => res.json(owner))
+    .catch((err) => res.status(400).json("Unable to find owner: " + err));
+};
+
+// PUT - update a single owner's cap
+const updateCap = async (req, res) => {
+  Owner.findById(idMap.ownersIDByName[req.params.ownerName])
+    .then((owner) => {
+      owner.cap.set(req.body.year, req.body.cap);
+      owner
+        .save()
+        .then(() =>
+          res.json(
+            `${owner.name} cap updated to ${req.body.cap} owner ${owner.cap}`
+          )
+        )
+        .catch((err) =>
+          res.status(400).json("Unable to update owner cap: " + err)
+        );
+    })
+    .catch((err) => res.status(400).json("Unable to find owner: " + err));
+};
+
+// PUT - update a single owner's penalty fee
+const updatePenaltyFee = async (req, res) => {
+  const { ownerName } = req.params;
+  await Owner.updateOne(
+    { _id: idMap.ownersIDByName[req.params.ownerName] },
+    req.body
+  )
+    .then(res.status(200).json(`${ownerName}'s penalty fee updated`))
+    .catch((err) => res.status(400).json("Unable to find owner: ", err));
+};
+
+module.exports = {
+  createOwner,
+  getOwners,
+  getOwnerByName,
+  updateCap,
+  updatePenaltyFee,
+};
--- backend/routes/owner.js
+++ backend/routes/owner.js
@@ -1,72 +1,25 @@
 const router = require("express").Router();
-const Owner = require("../models/owner.model");
-const idMap = require("../utils/idMaps.js");
+const {
+  createOwner,
+  getOwners,
+  getOwnerByName,
+  updateCap,
+  updatePenaltyFee,
+} = require("../controllers/owner.controller");
 
 // Get all owners
-router.route("/").get((req, res) => {
-  Owner.find()
-    .then((owner) => res.json(owner))
-    .catch((err) => res.status(400).json("Unable to find owner: " + err));
-});
+router.route("/").get(getOwners);
 
 // Get owner by name
-router.route("/:name").get((req, res) => {
-  Owner.find({ name: req.params.name })
-    .then((owner) => res.json(owner))
-    .catch((err) => res.status(400).json("Unable to find owner: " + err));
-});
+router.route("/:name").get(getOwnerByName);
 
 // Add new owner
-router.route("/add").post((req, res) => {
-  const { name, cap, sleeperId, active } = req.body;
-
-  Owner.create({
-    name,
-    cap,
-    sleeperId,
-    active,
-  })
-    .then(() =>
-      res.status(201).json({
-        message: "Owner created successfully!",
-        owner: {
-          name,
-          cap,
-          sleeperId,
-          active,
-        },
-      })
-    )
-    .catch((err) => res.status(400).json("Unable to add owner: " + err));
-});
+router.route("/add").post(createOwner);
 
 // Manually edit cap
-router.route("/updateCap/:ownerName").put((req, res) => {
-  Owner.findById(idMap.ownersIDByName[req.params.ownerName])
-    .then((owner) => {
-      owner.cap.set(req.body.year, req.body.cap);
-      owner
-        .save()
-        .then(() =>
-          res.json(
-            `${owner.name} cap updated to ${req.body.cap} owner ${owner.cap}`
-          )
-        )
-        .catch((err) =>
-          res.status(400).json("Unable to update owner cap: " + err)
-        );
-    })
-    .catch((err) => res.status(400).json("Unable to find owner: " + err));
-});
+router.route("/updateCap/:ownerName").put(updateCap);
 
 // Update penalty fee
-router.route("/updatePenaltyFee/:ownerName").put(async (req, res) => {
-  await Owner.updateOne(
-    { _id: idMap.ownersIDByName[req.params.ownerName] },
-    req.body
-  )
-    .then(res.status(200).json("Owner penalty fee updated"))
-    .catch((err) => res.status(400).json("Unable to find owner: ", err));
-});
+router.route("/updatePenaltyFee/:ownerName").put(updatePenaltyFee);
 
 module.exports = router;
