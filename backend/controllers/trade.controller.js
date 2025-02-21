413590d0212c891a0484832a392f97be28f0010a
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-20 19:08:03 -0500

    create trade.controller

---
+++ backend/controllers/trade.controller.js
@@ -0,0 +1,46 @@
+const Trade = require("../models/trade.model");
+const Player = require("../models/player.model");
+const idMap = require("../utils/idMaps.js");
+
+// GET all trades
+const getAllTrades = async (req, res) => {
+  Trade.find()
+    .then((trade) => res.json(trade))
+    .catch((err) => res.status(400).json("Unable to find trade: " + err));
+};
+
+// POST add a new trade
+const createTrade = async (req, res) => {
+  const { owner1, owner2, owner1_rec, owner2_rec, tradeNotes, years } =
+    req.body;
+
+  try {
+    await Player.updateMany(
+      { _id: { $in: owner1_rec.players } },
+      { $set: { owner: idMap.ownersIDByName[owner1] } }
+    );
+
+    await Player.updateMany(
+      { _id: { $in: owner2_rec.players } },
+      { $set: { owner: idMap.ownersIDByName[owner2] } }
+    );
+
+    const trade = await Trade.create({
+      owner1,
+      owner2,
+      owner1_rec,
+      owner2_rec,
+      tradeNotes,
+      years,
+    });
+
+    res.json({ message: "Trade added successfully!", trade: trade });
+  } catch (error) {
+    res.status(400).json("Unable to add trade: ", error);
+  }
+};
+
+module.exports = {
+  getAllTrades,
+  createTrade,
+};
--- backend/routes/trade.js
+++ backend/routes/trade.js
@@ -1,44 +1,14 @@
 const router = require("express").Router();
-const Trade = require("../models/trade.model");
-const Player = require("../models/player.model");
-const idMap = require("../utils/idMaps.js");
+
+const {
+  getAllTrades,
+  createTrade,
+} = require("../controllers/trade.controller");
 
 // Get all trades
-router.route("/").get((req, res) => {
-  Trade.find()
-    .then((trade) => res.json(trade))
-    .catch((err) => res.status(400).json("Unable to find trade: " + err));
-});
+router.route("/").get(getAllTrades);
 
 // Add a new trade
-router.route("/add").post(async (req, res) => {
-  const { owner1, owner2, owner1_rec, owner2_rec, tradeNotes, years } =
-    req.body;
-
-  try {
-    await Player.updateMany(
-      { _id: { $in: owner1_rec.players } },
-      { $set: { owner: idMap.ownersIDByName[owner1] } }
-    );
-
-    await Player.updateMany(
-      { _id: { $in: owner2_rec.players } },
-      { $set: { owner: idMap.ownersIDByName[owner2] } }
-    );
-
-    const trade = await Trade.create({
-      owner1,
-      owner2,
-      owner1_rec,
-      owner2_rec,
-      tradeNotes,
-      years,
-    });
-
-    res.json({ message: "Trade added successfully!", trade: trade });
-  } catch (error) {
-    res.status(400).json("Unable to add trade: ", error);
-  }
-});
+router.route("/add").post(createTrade);
 
 module.exports = router;
