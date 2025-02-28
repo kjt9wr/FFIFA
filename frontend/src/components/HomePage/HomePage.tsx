914807d7d1cfd4d5dfcb399b83407ebcbced6787
Kevin Thumim <kevin.j.thum@gmail.com>
2025-02-28 16:40:16 -0500

    add cursive font to homepage

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -4,8 +4,10 @@ import { Container } from "reactstrap";
 const HomePage = () => {
   return (
     <Container>
-      <h1 className="display-4">“Si vis pacem, para bellum”</h1>
-      <h1 className="display-4 ml-3">—If you want peace, prepare for war</h1>
+      <h1 className="display-4 cursiveFont">“Si vis pacem, para bellum”</h1>
+      <h1 className="display-4 ml-3 cursiveFont">
+        —If you want peace, prepare for war
+      </h1>
     </Container>
   );
 };
