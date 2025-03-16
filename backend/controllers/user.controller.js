54aa3d97327aa29f2450d59ceba1697fd9fe6da1
Kevin Thumim <kevin.j.thum@gmail.com>
2025-03-16 18:00:58 -0400

    sign token

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,11 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
 
 // login user
-
 const loginUser = async (req, res) => {
   res.json({ msg: "login user" });
 };
@@ -12,7 +16,9 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, username, password);
 
-    res.status(200).json({ email, username, user });
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, username, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
