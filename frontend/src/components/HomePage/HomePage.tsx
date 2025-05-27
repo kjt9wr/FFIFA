5478f1e7d3879ae82c1875600157a3065c46ddd7
Kevin Thumim <kevin.j.thum@gmail.com>
2025-05-27 15:27:12 -0400

    update homepage

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -3,11 +3,13 @@ import { Container } from "reactstrap";
 
 const HomePage = () => {
   return (
-    <Container>
-      <h1 className="display-4 cursiveFont">“Si vis pacem, para bellum”</h1>
-      <h1 className="display-4 ml-3 cursiveFont">
-        —If you want peace, prepare for war
+    <Container className="homepage-container">
+      <h1 className="homepage-quote cursiveFont">
+        “Si vis pacem, para bellum”
       </h1>
+      <h2 className="homepage-attribution cursiveFont">
+        —If you want peace, prepare for war
+      </h2>
     </Container>
   );
 };
