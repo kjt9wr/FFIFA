30b41fcb6f20198ef2454d0a6485b28d6f102a3e
Kevin Thumim <kevin.j.thum@gmail.com>
2025-08-31 19:22:37 -0400

    redesign the download button

--- frontend/src/components/Admin/GeneratePriceSheet.tsx
+++ frontend/src/components/Admin/GeneratePriceSheet.tsx
@@ -72,7 +72,17 @@ const GeneratePriceSheet = (props: GeneratePriceSheetProps) => {
           data={csvDto}
           headers={CSV_HEADERS}
           filename="ffifa_prices.csv"
+          className="download-csv-btn"
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 2a1 1 0 0 1 1 1v7.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4A1 1 0 1 1 6.293 8.293L8.5 10.586V3a1 1 0 0 1 1-1zm-7 13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 1 1 2 0v1a4 4 0 0 1-4 4H5a4 4 0 0 1-4-4v-1a1 1 0 1 1 2 0v1z" />
+          </svg>
           Download CSV
         </CSVLink>
       )}
